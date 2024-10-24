947fad92a12604e51c6a2250c1ec0811b1955dbc
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-10-24 11:26:18 -0300

    Criando e customizando components

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,12 @@
+import { Attendee } from "./components/attendee-list";
+import { Header } from "./components/header";
+
 export function App() {
 
   return (
-    <div className="flex gap-2">
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
+      <Header/>
+      <Attendee/>
     </div>
   )
 }
